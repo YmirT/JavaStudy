a31cf6e559370d30ed55e045354ca49b00e2119a
童彦铭 <tongyanming@guxiansheng.cn>
2022-03-02 18:01:24 +0800

    初始化

---
+++ ks/src/main/java/ymir/priv/App.java
@@ -0,0 +1,13 @@
+package ymir.priv;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
